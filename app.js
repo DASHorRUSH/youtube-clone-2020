3979998a8eb6b6f0897f9696b03a793bd7024adc
DASHorRUSH <67368023+DASHorRUSH@users.noreply.github.com>
2021-03-06 01:25:35 +0900

    Update 03.05

--- app.js
+++ app.js
@@ -3,21 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {userRouter} from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send('Hello from home');
 
 const handleProfile = (req, res) => res.send("You are on my profile");
-
-const betweenHome = (req, res, next) => {
-    console.log('Between');
-    next();  
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -25,10 +17,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use(betweenHome);
-
-app.get("/", betweenHome, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
