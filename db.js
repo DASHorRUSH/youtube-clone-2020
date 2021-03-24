3e1d8e94fe7cb664b350fcb22530d8da29ab047a
DASHorRUSH <67368023+DASHorRUSH@users.noreply.github.com>
2021-03-25 01:19:32 +0900

    Update 03.25

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:12121212,
+            name:"Nicolas",
+            email:"SHW98.com"
+        }
+    },
+    {
+        id:55555,
+        title: 'Video Nice',
+        description: 'This is something I love',
+        views:245,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:42132,
+            name:"Nicolas",
+            email:"SHW98.com"
+        }
+    },
+    {
+        id:1234,
+        title: 'Video Good',
+        description: 'This is something I love',
+        views:214,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:42112,
+            name:"Nicolas",
+            email:"SHW98.com"
+        }
+    },
+    {
+        id:5525,
+        title: 'Video Help',
+        description: 'This is something I love',
+        views:224,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:4215,
+            name:"Nicolas",
+            email:"SHW98.com"
+        }
+    }
+];
